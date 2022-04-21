6b8a490cf400471dadc505b423d4fde85ffca732
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-21 18:15:54 +0300

    Add cli.go file

---
+++ cli.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func wordcounter(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
+}
+
+func main() {
+	fmt.Print("Input words, then CTRL+C:")
+	fmt.Println(wordcounter(os.Stdin))
+}
