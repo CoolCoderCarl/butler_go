eaf6282e5cfbbbe9d202a1aaec95ea4ecb41f4ed
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-13 19:17:54 +0300

    Update cli.go

--- cli.go
+++ cli.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
 func fatal_err(err error) {
@@ -15,32 +16,66 @@ func fatal_err(err error) {
 	}
 }
 
-func clean_the_dir(directory_path string) {
+var target_dir_name string = "ALL"
 
-	dir, err := ioutil.ReadDir(directory_path)
+func clean_the_dir(args_cln_name string) {
+
+	dir, err := ioutil.ReadDir(args_cln_name)
 	fatal_err(err)
 	for _, d := range dir {
-		os.RemoveAll(path.Join([]string{directory_path, d.Name()}...))
+		os.RemoveAll(path.Join([]string{args_cln_name, d.Name()}...))
 	}
 }
 
-func group_up_files(new_dir_name string) {
+func group_up_files(args_dir_name string) {
 
-	print("New dir name " + new_dir_name)
+	files_extension := []string{
+		".txt",
+		".doc",
+		".docx",
+		".pdf",
+		".xlsx",
+		".bmp",
+		".jpg",
+		".rtf",
+		".pptx",
+		".conf",
+		".cfg",
+		".net",
+		".deny",
+		".allow",
+	}
+
+	files, err := ioutil.ReadDir(args_dir_name)
+	fatal_err(err)
+
+	for _, file := range files {
+		for _, ext := range files_extension {
+			if strings.HasSuffix(file.Name(), ext) {
+				new_dir_path := args_dir_name + target_dir_name + strings.ToUpper(ext)
+				if _, err := os.Stat(new_dir_path); os.IsNotExist(err) {
+					err := os.Mkdir(new_dir_path, 0644)
+					fatal_err(err)
+				}
+				err := os.Rename(args_dir_name+file.Name(), new_dir_path+"/"+file.Name())
+				fatal_err(err)
+			}
+		}
+	}
 }
 
 func main() {
-	directory_path := flag.String("clean", ".", "Clean target directory")
-	target_dir_name := flag.String("dir", ".", "Dir to group up the files")
+	args_cln_name := flag.String("clean", ".", "Clean target directory")
+	args_dir_name := flag.String("dir", ".", "Dir to group up the files")
 	flag.Parse()
 
 	switch os.Args[1] {
 	case "--clean":
-		fmt.Print(*directory_path)
-		clean_the_dir(*directory_path)
+		fmt.Print(*args_cln_name)
+		clean_the_dir(*args_cln_name)
 	case "--dir":
-		fmt.Println(*target_dir_name)
-		group_up_files(*target_dir_name)
+		fmt.Println(*args_dir_name)
+		group_up_files(*args_dir_name)
 	default:
 		os.Exit(1)
 	}
