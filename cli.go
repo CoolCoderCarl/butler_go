50a45444094641546cfdd203055cf568408f18c7
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-13 12:47:34 +0300

    Update cli.go

--- cli.go
+++ cli.go
@@ -24,9 +24,24 @@ func clean_the_dir(directory_path string) {
 	}
 }
 
+func group_up_files(new_dir_name string) {
+
+	print("New dir name " + new_dir_name)
+}
+
 func main() {
-	directory_path := flag.String("clean", ".", "Remove files in target directory")
+	directory_path := flag.String("clean", ".", "Clean target directory")
+	target_dir_name := flag.String("dir", ".", "Dir to group up the files")
 	flag.Parse()
-	fmt.Print(*directory_path)
-	clean_the_dir(*directory_path)
+
+	switch os.Args[1] {
+	case "--clean":
+		fmt.Print(*directory_path)
+		clean_the_dir(*directory_path)
+	case "--dir":
+		fmt.Println(*target_dir_name)
+		group_up_files(*target_dir_name)
+	default:
+		os.Exit(1)
+	}
 }
