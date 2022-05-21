ebc2319744487cb7e21645fba6f815869c365daa
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-21 21:50:56 +0300

    Add clean func; update flow

--- butler_go.go
+++ butler_go.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"path"
 )
 
-func cleanDir() {
+func cleanDir(argPathToClean string) {
 
-	pathDir, err := os.ReadDir("/test/test/")
+	pathDir, err := os.ReadDir(argPathToClean)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, fileName := range pathDir {
-		os.RemoveAll(path.Join([]string{("/test/test/"), fileName.Name()}...))
+		os.RemoveAll(path.Join([]string{(argPathToClean), fileName.Name()}...))
 	}
 
 }
 
 func main() {
-	cleanDir()
+	argPathToClean := flag.String("clean", ".", "Remove all files from target directory")
+	flag.Parse()
+
+	switch os.Args[1] {
+	case "--clean":
+		print(*argPathToClean)
+		cleanDir(*argPathToClean)
+	}
+
 	// FUNC TO GROUP UP
 }
