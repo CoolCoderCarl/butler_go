e523f414e56cbdcb37fefaf70375610023834544
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-21 22:35:05 +0300

    Add group up file func; update flows

--- butler_go.go
+++ butler_go.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
 func check(err error) {
@@ -24,15 +26,38 @@ func cleanDir(argPathToClean string) {
 
 }
 
+func groupUpFiles(argGroupUpFiles string) {
+	extensions := []string{".docx", ".pptx", ".txt", ".xlsx"}
+
+	files, err := os.ReadDir(argGroupUpFiles)
+	check(err)
+
+	for _, file := range files {
+		for _, ext := range extensions {
+			if strings.HasSuffix(file.Name(), ext) {
+				newDir := argGroupUpFiles + "/" + "ALL_" + strings.ToUpper(ext)
+				if _, err := os.Stat(newDir); os.IsNotExist(err) {
+					err := os.Mkdir(newDir, 0644)
+					check(err)
+				}
+				err := os.Rename(argGroupUpFiles+"/"+file.Name(), newDir+"/"+file.Name())
+				check(err)
+			}
+		}
+	}
+}
+
 func main() {
 	argPathToClean := flag.String("clean", ".", "Remove all files from target directory")
+	argGroupUpFiles := flag.String("group", ".", "Group up all files in target directory")
 	flag.Parse()
 
 	switch os.Args[1] {
 	case "--clean":
-		print(*argPathToClean)
+		fmt.Println(*argPathToClean)
 		cleanDir(*argPathToClean)
+	case "--group":
+		fmt.Println(*argGroupUpFiles)
+		groupUpFiles(*argGroupUpFiles)
 	}
-
-	// FUNC TO GROUP UP
 }
